f17537f0c87150e02f0ebb3d3d20e0391a7999d9
bruskiwang <bruskiwang@tencent.com>
2021-12-12 17:24:24 +0800

    feat: 实现 useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import React from 'react';
-import useState from "./useState";
+import ReactDOM from "react-dom";
+
+function render() {
+  stateIndex = 0;  // 重新渲染时归零
+  effectIndex = 0;  // effects下标归零
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+let states = [];
+let setters = [];
+let stateIndex = 0;
+
+function createSetter(index) {
+  return function (value) {
+    states[index] = value;
+    render();
+  }
+}
+
+function useState(initialState) {
+  const existState = states[stateIndex];
+  if (!existState) {
+    states.push(initialState);
+  }
+  setters.push(createSetter(stateIndex));
+  const state = states[stateIndex];
+  const setState = setters[stateIndex];
+  stateIndex ++;
+  return [state, setState];
+}
+
+let prevDepsAry = [];
+let effectIndex = 0;
+
+function useEffect(callback, depsAry) {
+  if (Object.prototype.toString.call(callback) !== '[object Function]') {
+    throw new Error('useEffect第一个参数必须是函数')
+  }
+  if (typeof depsAry === 'undefined') {
+    callback();
+  } else {
+    if (Object.prototype.toString.call(depsAry) !== '[object Array]') {
+      throw new Error('useEffect第二个参数必须是数组')
+    }
+
+    const prevDeps = prevDepsAry[effectIndex];
+
+    // 同步依赖值
+    const hasChanged = !prevDeps ? true : prevDeps.some((dep, index) => dep !== depsAry[index]);
+    if (hasChanged) {
+      callback()
+    }
+    prevDepsAry[effectIndex] = depsAry;
+    effectIndex ++;
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState('张三');
+
+  useEffect(() => {
+    console.log('hello');
+  }, [])
+
+  useEffect(() => {
+    console.log('world');
+  }, [])
+
   return <div>
     {count}
     <button onClick={() => setCount(count + 1)}>setCount</button>
